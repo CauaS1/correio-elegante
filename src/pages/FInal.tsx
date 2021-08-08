d68faea8a4bd0f9cced7d9fe4a5ad568806644d0
CauaS1 <semnet2404@gmail.com>
2021-08-08 12:44:30 -0300

    Final screen finished

--- src/pages/FInal.tsx
+++ src/pages/FInal.tsx
@@ -1,25 +1,146 @@
-import React from 'react';
-import { Text, View, StyleSheet, StatusBar } from 'react-native';
+import React, { useContext } from 'react';
+import { Text, View, StyleSheet, StatusBar, Image } from 'react-native';
+import { FontContext } from '../contexts/FontContext';
+import { LinearGradient } from 'expo-linear-gradient';
+
+import AppLoading from 'expo-app-loading';
 
 function Final() {
-  return (
-    <View style={styles.container}>
-      <StatusBar translucent={false} backgroundColor="#fff" />
+  const { fontsLoaded } = useContext(FontContext);
 
-      <View style={styles.about}>
-        <Text>Eae</Text>
+  if (!fontsLoaded) {
+    return <AppLoading />
+  } else {
+    return (
+      <View style={styles.container}>
+        <StatusBar translucent={false} backgroundColor="#fff" />
 
-        <Text>Seu correio foi enviado com sucesso</Text>
+        <View style={styles.about}>
+          <Image source={require('../../assets/icons/check.png')} style={styles.icon} />
+
+          <Text style={styles.resultText}>Seu correio foi enviado com sucesso</Text>
+        </View>
+
+        <View style={styles.cardContainer}>
+
+          <View style={styles.card}>
+
+            <View style={styles.cardSides}>
+              <View style={styles.cardIconsContainer}>
+                <Image source={require('../../assets/icons/fast-food.png')} style={styles.cardIcons} />
+              </View>
+            </View>
+
+            <View style={styles.cardMiddle}>
+              <View style={styles.cardTextContainer}>
+                <Text style={styles.cardText}>De: Admirador Secreto</Text>
+              </View>
+
+              <View style={styles.cardTextContainer}>
+                <Text style={styles.cardText}>Para: Você</Text>
+              </View>
+            </View>
+
+            <View style={styles.cardSides}>
+              <View style={styles.cardIconsContainer}>
+                <Image source={require('../../assets/icons/fast-food.png')} style={styles.cardIcons} />
+              </View>
+            </View>
+          </View>
+        </View>
       </View>
-    </View>
-  )
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff'
+  },
+
+  /* A B O U T */
+  about: {
+    width: '100%',
+    height: '25%',
+
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  icon: {
+    width: 50,
+    height: 50,
+
+    marginBottom: 15
+  },
+  resultText: {
+    fontSize: 30,
+    lineHeight: 35,
+
+    textAlign: 'center',
+    fontFamily: 'Poppins-Medium',
+
+    color: '#2F394B'
+  },
+
+  /* C A R D  C O N T A I N E R */
+  cardContainer: {
+    width: '100%',
+    height: '75%',
+
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  card: {
+    width: '80%',
+    height: '85%',
+
+    borderRadius: 20,
+
+    backgroundColor: '#E06C88',
+    justifyContent: 'space-around',
+    overflow: 'hidden'
+  },
+
+  cardSides: {
+    height: '30%',
+    alignItems: 'center',
+    padding: 15,
+  },
+
+  cardMiddle: { /* M I D D L E  */
+    padding: 10,
+
+    flexDirection: 'row',
+    justifyContent: 'center'
+
+  },
+  cardTextContainer: {
+    width: '90%',
+
+    transform: [{ rotate: '270deg' }]
+  },
+  cardText: {
+    textAlign: 'center',
+    fontFamily: 'Poppins-Light',
+    color: '#E7DFDD',
+    fontSize: 20,
+  },
+
+  cardIconsContainer: {
+    width: 130,
+    height: 130,
+    backgroundColor: '#fff',
+    borderRadius: 70,
+
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cardIcons: {
+    width: 70,
+    height: 70
   }
 })
+
 
 export default Final;
